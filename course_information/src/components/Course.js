4b82fbfc60c0816b1354a40e916ca74598c91978
Mike-ops273 <mivaxd55@yahoo.com>
2022-04-11 10:57:02 +0300

    improved 2.5

--- course_information/src/components/Course.js
+++ course_information/src/components/Course.js
@@ -4,44 +4,37 @@ const Course = (props) => {
   return (
     <div>
       <h2>{props.courses[0].name}</h2>
-      {/*sending just the first parts array to <Content>*/}
-      <Content1 parts1={props.courses[0].parts} />
-      {console.log(props.courses[1].parts)}
+      <Content
+        part={props.courses[0].parts.map(({ name, exercises, id }) => (
+          <p key={id}>
+            {name} {exercises}
+          </p>
+        ))}
+      />
+      <Total
+        total={props.courses[0].parts.reduce((previous, current) => {
+          return previous + current.exercises;
+        }, 0)}
+      />
       <h2>{props.courses[1].name}</h2>
-      <Content2 parts2={props.courses[1].parts} />
+      <Content
+        part={props.courses[1].parts.map(({ name, exercises, id }) => (
+          <p key={id}>
+            {name} {exercises}
+          </p>
+        ))}
+      />
+      <Total
+        total={props.courses[1].parts.reduce((previous, current) => {
+          return previous + current.exercises;
+        }, 0)}
+      />
     </div>
   );
 };
 
-const Content1 = (props) => (
-  <>
-    {props.parts1.map(({ name, exercises, id }) => (
-      <p key={id}>
-        {name} {exercises}
-      </p>
-    ))}
-    <Total
-      total={props.parts1.reduce((previous, current) => {
-        return previous + current.exercises;
-      }, 0)}
-    />
-  </>
-);
-const Content2 = (props) => {
-  return (
-    <>
-      {props.parts2.map(({ name, exercises, id }) => (
-        <p key={id}>
-          {name} {exercises}
-        </p>
-      ))}
-      <Total
-        total={props.parts2.reduce((previous, current) => {
-          return previous + current.exercises;
-        }, 0)}
-      />
-    </>
-  );
+const Content = (props) => {
+  return <>{props.part}</>;
 };
 
 const Total = (props) => {
