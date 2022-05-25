bd17cbf12fbb363777b74743ad6d0de8890a3e2d
Mike-ops <mivaxd55@yahoo.com>
2022-05-25 15:13:12 +0300

    improved 2.12

--- countries/src/components/Countries.js
+++ countries/src/components/Countries.js
@@ -8,9 +8,11 @@ const Countries = ({ countries, query }) => {
   console.log(countries);
   if (query == "") {
     return <>{""}</>;
-  } else if (queryCountries.length > 10) { //if too many results
+  } else if (queryCountries.length > 10) {
+    //if too many results
     return <>Too many countries, please be more specific</>;
-  } else if (queryCountries.length <= 10 && queryCountries.length > 1) { //if 10 results or less
+  } else if (queryCountries.length <= 10 && queryCountries.length > 1) {
+    //if 10 results or less
     console.log("in here");
     console.log(queryCountries);
     return (
@@ -20,34 +22,43 @@ const Countries = ({ countries, query }) => {
         ))}
       </>
     );
-  } else if (queryCountries.length == 1) { //a singular result
+  } else if (queryCountries.length == 1) {
+    //a singular result
     return (
       <>
-        {console.log("length inside", queryCountries.length)}
-        {console.log(queryCountries)}
-        {queryCountries.map((country) => (
-          <ul key={country.name.common}>
-            <h2>{country.name.common}</h2>
-            <li>capital {country.capital}</li>
-            <li>area: {country.area}</li>
-          </ul>
-        ))}
-        <h2>Languages</h2>
-        {console.log(queryCountries[0].languages)}
-        {/*queryCountries[0].languages*/}
-        {Object.values(queryCountries[0].languages).map((value, index) => {
-          return (
-            <div key={index}>
-              <p>{value}</p>
-            </div>
-          );
-        })}
-
-        <h1>{/*queryCountries[0].flag*/}</h1>
-        <img src={queryCountries[0].flags.png} />
+        <SingleCountry queryCountries={queryCountries} />
       </>
     );
   }
+};
+
+const SingleCountry = ({ queryCountries }) => {
+  return (
+    <>
+      {console.log("length inside", queryCountries.length)}
+      {console.log(queryCountries)}
+
+      {queryCountries.map((country) => (
+        <ul key={country.name.common}>
+          <h2>{country.name.common}</h2>
+          <li>capital {country.capital}</li>
+          <li>area: {country.area}</li>
+        </ul>
+      ))}
+      <h2>Languages</h2>
+      {console.log(queryCountries[0].languages)}
+      {/*queryCountries[0].languages*/}
+      {Object.values(queryCountries[0].languages).map((value, index) => {
+        return (
+          <div key={index}>
+            <p>{value}</p>
+          </div>
+        );
+      })}
+      <h1>{/*queryCountries[0].flag*/}</h1>
+      <img src={queryCountries[0].flags.png} />
+    </>
+  );
 };
 
 export default Countries;
